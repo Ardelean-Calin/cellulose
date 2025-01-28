daf9b98e702c806c9e57f8a26b98e51045e12966
Ardelean Călin <9417983+Ardelean-Calin@users.noreply.github.com>
2025-01-28 21:44:45 +0200

    Added tags menu

---
+++ README.md
@@ -0,0 +1,6 @@
+### TODO
+
+Using Ollama/OpenAI APIs we can do some cool LLM stuff:
+- [ ] Generate short description for each document using LLMs
+- [ ] Generate tags for each document using LLMs (as in, choose from the available tags)
+- [ ] Move front-end functionality to the server side (for example, the tags menu filtering logic)
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sort"
+	"strings"
 	"text/template"
 
 	"github.com/Ardelean-Calin/cellulose/internal/data"
@@ -10,10 +12,35 @@ import (
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
 	documents := data.GetSampleDocuments()
+	allTags := getAllUniqueTags(documents)
+
 	tmpl := template.Must(template.ParseFiles("templates/home.html"))
 	tmpl.Execute(w, map[string]interface{}{
 		"Documents": documents,
+		"AllTags":   allTags,
 	})
+}
+
+// Helper function to get unique tags from documents
+func getAllUniqueTags(documents []data.Document) []data.Tag {
+	tagMap := make(map[string]data.Tag)
+	for _, doc := range documents {
+		for _, tag := range doc.Tags {
+			tagMap[tag.Name] = tag
+		}
+	}
+
+	uniqueTags := make([]data.Tag, 0, len(tagMap))
+	for _, tag := range tagMap {
+		uniqueTags = append(uniqueTags, tag)
+	}
+
+	// Sort tags by name (case-insensitive)
+	sort.Slice(uniqueTags, func(i, j int) bool {
+		return strings.ToLower(uniqueTags[i].Name) < strings.ToLower(uniqueTags[j].Name)
+	})
+
+	return uniqueTags
 }
 
 func main() {
