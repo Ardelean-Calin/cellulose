4e28f44a0c3d257887379f3b8fc3d72e5c7910dc
Ardelean Călin <9417983+Ardelean-Calin@users.noreply.github.com>
2025-02-07 15:57:00 +0200

    feat: add status code to Logging middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -3,11 +3,28 @@ package middleware
 import (
 	"log"
 	"net/http"
+	"time"
 )
+
+type wrappedWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (w *wrappedWriter) WriteHeader(statusCode int) {
+	w.ResponseWriter.WriteHeader(statusCode)
+	w.statusCode = statusCode
+}
 
 func Logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s", r.Method, r.URL.Path)
-		next.ServeHTTP(w, r)
+		start := time.Now()
+		wrapped := &wrappedWriter{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+
+		next.ServeHTTP(wrapped, r)
+		log.Println(wrapped.statusCode, r.Method, r.URL.Path, time.Since(start))
 	})
 }
